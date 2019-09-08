b523618ccd2e9bc6bcdee68cdd32be91b96b2c7e
Jas <jas03x@gmail.com>
2019-09-08 01:23:58 -0400

    Change of design - we actually won't mix c and asm
    The main idea behind this design decision is that assembly and C are completely different mechanisms... we want to keep things clean. C is controlled by the compiler, and assembly is controlled completely by the programmar. We do not want the mess of mixing platform specific code into intermediate code which is spat out by the compiler...

--- test/main.c
+++ test/main.c
@@ -1,21 +1,4 @@
 
-const BOOTLOADER_SIZE = 512;
-
-asm BOOTLOADER
-{
-BOOTLOADER_START:
-    // read the kernel from the disk
-    MOV(R01,   2); // operation = 2 (read)
-    MOV(R02,   9); // start at sector 9 since the first 8 sectors are the bootloader
-    MOV(R03,   8); // read 8 sectors, which is 512 bytes (the kernel is 512 bytes at the moment)
-    MOV(R04, 512); // the kernel will be loaded into the location right after the bootloader
-
-    JMP(kernel_entry);
-
-BOOTLOADER_END:
-    PAD(BOOTLOADER_SIZE - (BOOTLOADER_END - BOOTLOADER_START));
-}
-
 void kernel_entry()
 {
     // infinite loop
