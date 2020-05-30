b3ed41bfd95fab5d9d7dc40c6d009edeaa791864
Jas03x <jas03x@gmail.com>
2020-05-30 15:54:19 -0400

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,7 +1,22 @@
 
-const struct ret { U32 sum, mul; } *inst0 = 0, *inst1 = 0;
+//#include <stdio.h>
+//typedef unsigned int U32;
 
-U32 main(U32 argc, const U8** argv)
+U32 sqr(U32 i)
 {
-    return 0;
+	return i * i;
+}
+
+U32 (*get_pfn(void))(U32)
+{
+	return sqr;
+}
+
+int main(void)
+{
+	U32(*pfn_sqr)(U32) = &sqr;
+	U32(**ppfn_sqr)(U32) = &pfn_sqr;
+
+	printf("%u\n", get_pfn()(2));
+	return 0;
 }
